0dc34655934028d980bac71e4a9b37db9873128c
Dhanush Kumar <dk652003@gmail.com>
2024-02-16 12:47:28 +0530

    Sorting a list of numbers based on tens Place

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,24 @@
-import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        List<Integer> num = Arrays.asList(21,45,76,34,98,67,43);
-
-        int result = num.stream()
-                .distinct()
-                .sorted()
-                .skip(1)
-                .findFirst()
-                .orElse(0);
-        System.out.println("The second smallest Element is: "+ result);
-
-        List<Integer> num1 = Arrays.asList(21,45,76,34,98,67,43);
-
-        int result1 = num1.stream()
-                .distinct()
-                .sorted(Comparator.reverseOrder())
-                .skip(1)
-                .findFirst().orElse(0);
-        System.out.println("The Second Largest Element is: " + result1);
+        List<Integer> num = Arrays.asList(233,569,442,760,226,478,197);
+        Comparator<Integer> coms = new Comparator<Integer>() {
+            @Override
+            public int compare(Integer i, Integer j) {
+                if(i %100 < j%100){
+                    return -1;
+                }
+                else
+                    return 1;
+            }
+        };
+        num.sort(coms);
+        for(int n : num){
+            System.out.println(n);
+        }
     }
 }
