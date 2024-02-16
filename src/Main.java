123016a812f57611a21c92d6479a600522b07fad
Dhanush Kumar <dk652003@gmail.com>
2024-02-16 10:14:29 +0530

    Added Method To Sort According To The Price Of The Phone

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List<phone> phoneSort = new ArrayList<>();
+        //phone ob =new phone("a",799,4);
+
+        phoneSort.add(new phone ("Samsung",75000,8));
+        phoneSort.add(new phone("oppo",90000,16));
+        phoneSort.add(new phone("Vivo",80000,8));
+        phoneSort.add(new phone("Apple",65000,4));
+
+        List<phone> result = phoneSort.stream()
+                .sorted(Comparator.comparing(phone::getPrice))
+                .toList();
+        result.forEach(n-> System.out.println(n));
     }
 }
